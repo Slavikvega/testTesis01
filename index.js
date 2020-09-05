e83a1408bb9782e7e80acc5dc29e9c3df3073856
Slavikvega <esusunaga@centro.edu.mx>
2020-09-05 14:31:24 -0500

    commit inicial

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 5500;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+ app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.get('/home', (req, res) => res.render('pages/index'));
+  //app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  http.listen(PORT, () => {
+  console.log('go to htto//localHost:5500');
+})
+
+// agregar rutas
+  app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
